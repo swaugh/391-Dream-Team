452e6194ae4cf3893c82bf6c5c3df6616c7d1a10
Seth Waugh <s.waugh@me.com>
2016-02-02 15:03:41 -0800

    Refinement To OE Code
    No Errors on this design, unsure if runs correctly (not checked
    w/cable).

--- Optical_Encoder_POC/Optical_Encoder_POC.ino
+++ Optical_Encoder_POC/Optical_Encoder_POC.ino
@@ -1,42 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+
   double optical_angle = 0;
-  Serial.begin(9600);
   int Channel_A = 0;
   int Channel_B = 0;
   int count_encoder = 0;
-  char OE_direction[9];
+  double OE_direction;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Channel_A = digitalread(5);
-  Channel_B = digitalread(4);
+  Channel_A = digitalRead(5);
+  Channel_B = digitalRead(4);
 
-  if Channel_A == HIGH{
+  if (Channel_A == HIGH) {
     count_encoder = count_encoder + 1;
-    if Channel_B == LOW
-      OE_direction = "forward";
-    else if Channel_B == HIGH
-      OE_direction = "backward";
+    if (Channel_B == LOW)
+      OE_direction = 1;
+    else if (Channel_B == HIGH)
+      OE_direction = -1;
     while(Channel_A == HIGH){
        //ISR DOES STUFF
       }
-    if Channel_B == LOW
-      OE_direction = "backward";
-    else if Channel_B == HIGH
-      OE_direction = "forward";
+    if (Channel_B == LOW)
+      OE_direction = -1;
+    else if (Channel_B == HIGH)
+      OE_direction = 1;
   }
-  if OE_direction == "forward"
+  if (OE_direction == 1)
      optical_angle = optical_angle + 3.6;
-  else if OE_diection == "backward";
+  else if (OE_direction == -1);
     optical_angle = optical_angle - 3.6;
 
-  if optical_angle > 360
+  if (optical_angle > 360)
     optical_angle = optical_angle - 360;
-  else if optical_angle < -360
+  else if (optical_angle < -360)
     optical_angle = optical_angle + 360;
 
-  print("direction = %s , angle = %d \n", OE_direction, optical_angle);
-    
+  Serial.print(OE_direction);
+  Serial.print('\N');
+  Serial.print(optical_angle);
 }
