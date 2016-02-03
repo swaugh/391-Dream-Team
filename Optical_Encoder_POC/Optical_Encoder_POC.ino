bf20a88f6fa32be02fc7dbf6ce0ca0eb63c9a7ed
Alex Abulnaga <alex.abulnaga@gmail.com>
2016-02-02 16:32:50 -0800

    reduced_redundancies in printing

--- Optical_Encoder_POC/Optical_Encoder_POC.ino
+++ Optical_Encoder_POC/Optical_Encoder_POC.ino
@@ -14,7 +14,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(5) == HIGH) {
     count_encoder = count_encoder + 1;
-    delayMicroseconds(10);
+    delayMicroseconds(100);
     if (digitalRead(4) == LOW)
       OE_direction = 1;
     else if (digitalRead(4) == HIGH)
@@ -22,23 +22,25 @@ void loop() {
   while(digitalRead(5) == HIGH){
        //ISR DOES STUFF
       }
-    if (OE_direction == 1)
+    if (OE_direction == 1){
        optical_angle = optical_angle + 3.6;
-    else if (OE_direction == -1)
+       Serial.print("Moving Forwards, angle = :  ");
+    }
+    else if (OE_direction == -1){
       optical_angle = optical_angle - 3.6;
+       Serial.print("Moving Backwards, angle = :  ");
+    }
   }
   if (optical_angle > 360)
     optical_angle = optical_angle - 360;
   else if (optical_angle < -360)
     optical_angle = optical_angle + 360;
-  if (OE_direction = 1)
-    Serial.print("Moving Forwards, angle = :  ");
-  else if (OE_direction = -1)
-    Serial.print("Moving Backwards, angle = :  ");
+
     
    Serial.print(optical_angle);
+   Serial.print('\t');
+   Serial.print("degrees");
    Serial.print('\n');
-   Serial.print(count_encoder);
-   Serial.print('\n');
+
    
 }
