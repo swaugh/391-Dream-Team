5a0859b1548af9c6c838e91fb8729d6751dbe2fd
Alex Abulnaga <alex.abulnaga@gmail.com>
2016-03-02 09:48:49 -0800

    Optical encoer added
    Optical encoder is now working with the rotary encoder

--- rotary_encoder_ISR/rotary_encoder_ISR.ino
+++ rotary_encoder_ISR/rotary_encoder_ISR.ino
@@ -1,13 +1,20 @@
 
-  volatile double RE_angle; //Rotary encoder angle
+  volatile double RE_angle; //rotary encoder angle
   volatile int RE_dir; //rotary encoder direction
   const int RE_lead_pin = 3;
-  const int RE_lag_pin = 13; 
+  const int RE_lag_pin = 13;
+
+  volatile double OE_angle; //optical encoder angle
+  volatile int OE_dir; //optical encoder direction
+  const int OE_lead_pin = 2;
+  const int OE_lag_pin = 12; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(RE_lag_pin, INPUT);
+  pinMode(OE_lag_pin, INPUT);
+  attachInterrupt(0, OE_ISR, RISING); //optical encoder isr, will trigger on rising edge of channel A
   attachInterrupt(1, RE_ISR, RISING); //rotary encoder isr, will trigger on rising edge of channel A
 }
 
@@ -15,6 +22,36 @@ void loop() {
   // put your main code here, to run repeatedly:
   delay(1);
 }
+
+ void OE_ISR (){
+  
+      if (digitalRead(OE_lag_pin) == HIGH){
+      OE_dir = -1;
+      OE_angle = OE_angle - 3.6;
+      if (OE_angle <= -360)
+          OE_angle = OE_angle + 360;
+          
+       Serial.print("optical Moving Backwards, angle = :  ");
+       Serial.print(OE_angle);
+       Serial.print('\t');
+       Serial.print("degrees");
+       Serial.print('\n');
+    }
+
+    else if (digitalRead(OE_lag_pin) == LOW){
+      OE_dir = 1;
+      OE_angle = OE_angle + 3.6;
+       if (OE_angle >= 360)
+         OE_angle = OE_angle - 360;
+
+       Serial.print("optical Moving Forwards, angle = :  ");
+       Serial.print(OE_angle);
+       Serial.print('\t');
+       Serial.print("degrees");
+       Serial.print('\n');
+       
+    }
+ }
 
  void RE_ISR (){
   
@@ -24,7 +61,7 @@ void loop() {
       if (RE_angle <= -360)
           RE_angle = RE_angle + 360;
           
-       Serial.print("Moving Backwards, angle = :  ");
+       Serial.print("rotary Moving Backwards, angle = :  ");
        Serial.print(RE_angle);
        Serial.print('\t');
        Serial.print("degrees");
@@ -37,7 +74,7 @@ void loop() {
        if (RE_angle >= 360)
          RE_angle = RE_angle - 360;
 
-       Serial.print("Moving Forwards, angle = :  ");
+       Serial.print("rotary Moving Forwards, angle = :  ");
        Serial.print(RE_angle);
        Serial.print('\t');
        Serial.print("degrees");
