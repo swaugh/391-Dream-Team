4f01f29ebdbee0da08f76ddb1040757b32c2c91e
Alex Abulnaga <alex.abulnaga@gmail.com>
2016-03-01 17:01:38 -0800

    Finished adding rotary
    The rotary encoder is working completely in the ISR

--- rotary_encoder_ISR/rotary_encoder_ISR.ino
+++ rotary_encoder_ISR/rotary_encoder_ISR.ino
@@ -1,39 +1,27 @@
 
-  double RE_angle; //Rotary encoder angle
-  int RE_dir; //rotary encoder direction
-  const int RE_lead_pin = 5;
+  volatile double RE_angle; //Rotary encoder angle
+  volatile int RE_dir; //rotary encoder direction
+  const int RE_lead_pin = 3;
   const int RE_lag_pin = 13; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(RE_lag_pin, INPUT);
+  attachInterrupt(1, RE_ISR, RISING); //rotary encoder isr, will trigger on rising edge of channel A
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(RE_lead_pin) == HIGH) {
-    if (digitalRead(RE_lag_pin) == LOW)
-      RE_dir = 1;
-    else if (digitalRead(RE_lag_pin) == HIGH)
+  delay(1);
+}
+
+ void RE_ISR (){
+  
+      if (digitalRead(RE_lag_pin) == HIGH){
       RE_dir = -1;
-  while(digitalRead(RE_lead_pin) == HIGH){
-       //ISR DOES STUFF
-      }
-    if (RE_dir == 1){
-       RE_angle = RE_angle + 15;
-       if (RE_angle > 360)
-          RE_angle = RE_angle - 360;
-          
-       Serial.print("Moving Forwards, angle = :  ");
-       Serial.print(RE_angle);
-       Serial.print('\t');
-       Serial.print("degrees");
-       Serial.print('\n');
-    }
-    else if (RE_dir == -1){
       RE_angle = RE_angle - 15;
-      if (RE_angle < -360)
+      if (RE_angle <= -360)
           RE_angle = RE_angle + 360;
           
        Serial.print("Moving Backwards, angle = :  ");
@@ -42,7 +30,20 @@ void loop() {
        Serial.print("degrees");
        Serial.print('\n');
     }
-  }
-  delayMicroseconds(125);
-  
-}
+
+    else if (digitalRead(RE_lag_pin) == LOW){
+      RE_dir = 1;
+      RE_angle = RE_angle + 15;
+       if (RE_angle >= 360)
+         RE_angle = RE_angle - 360;
+
+       Serial.print("Moving Forwards, angle = :  ");
+       Serial.print(RE_angle);
+       Serial.print('\t');
+       Serial.print("degrees");
+       Serial.print('\n');
+       
+    }
+ }
+
+
