e4b3a80b28ed83b38bf3fa271e46e4186126599a
Alex Abulnaga <alex.abulnaga@gmail.com>
2016-03-10 21:15:41 -0800

    Working kinda!
    @@@

--- pid_phase_0/pid_phase_0.ino
+++ pid_phase_0/pid_phase_0.ino
@@ -19,7 +19,7 @@
   //OE PID variables
    double RE_Setpoint, RE_PID_Input, RE_PID_Output;
    int RE_Count = 0;
-   PID RE_PID(&RE_PID_Input, &RE_PID_Output, &RE_Setpoint,1.25,0.075,0.25, DIRECT); // in, out, Kp, Ki, Kd, direction Best So far 
+   PID RE_PID(&RE_PID_Input, &RE_PID_Output, &RE_Setpoint,0.1,0.02,0, DIRECT); // in, out, Kp, Ki, Kd, direction Best So far 1.25,0.075,0.25
 
 void setup() {
   // put your setup code here, to run once:
@@ -37,41 +37,66 @@ void setup() {
 
   //initalize Rotary PID
   RE_PID_Input = RE_angle; //input to RE PID controller is the actal measured angle
-  RE_Setpoint = 0; //for now we set a test setpoint, test on -36,-72
+  RE_Setpoint = 180; //for now we set a test setpoint, test on -36,-72
   RE_PID.SetMode(AUTOMATIC); //turn on the PID to automatic mode
-  RE_PID.SetOutputLimits(140, 243); //cap the output of the RE_PID to 128 (full reverse) to 255 (full forward), 192 is stopped
+  RE_PID.SetOutputLimits(192, 240); //cap the output of the RE_PID to 128 (full reverse) to 255 (full forward), 192 is stopped
 }
 
 void loop() {
   // Optical encoder angle should never be negative (starts at 0 deg)
-   if (OE_angle < 0)
+   if (OE_angle <= 0)
    {
     OE_angle = 0;
    }
    //Compute PID output for OE and RE
+  
    OE_PID_Input = OE_angle; //input to OE PID controller is the actal measured angle
    OE_PID.Compute();
    
    RE_PID_Input = RE_angle; //input to RE PID controller is the actal measured angle
    RE_PID.Compute();
-   
+
+
+   if (RE_angle >= RE_Setpoint){
+      Serial.write(0);
+   }
+   else{
    //write output to motor 1 (Lift)
    Serial.write((int)OE_PID_Output);
    //delayMicroseconds(50); //delay unecessary??
    //Write to motor 2 (Yaw)
-   Serial.write((int) RE_PID_Output);
+   //Serial.write((int) RE_PID_Output);
 
-     if (OE_angle == OE_Setpoint)
+   /*if(OE_Count <1000){
+    Serial.write (192);
+   }
+   else{
+   */
+    Serial.write((int) RE_PID_Output);
+   }
+
+    /* if (OE_angle == OE_Setpoint){
+      RE_Setpoint = 180;
+     }
+     */
+    /* 
+     if (OE_angle == OE_Setpoint){
       OE_Count = OE_Count + 1; 
+     }
       
-    if (OE_Count == 1000)  //can vary this value, probably need to play with delays to get a good number
+    if (OE_Count == 1000){  //can vary this value, probably need to play with delays to get a good number
        RE_Setpoint = 180;  //Perform 180
+    }
        
-    if (RE_angle == RE_Setpoint)
+    if (RE_angle == RE_Setpoint){
       RE_Count = RE_Count + 1;
-     
-    if (RE_Count == 1000)  //Vary this based on timings
-      RE_Setpoint = 0; //Lower arm to base position
+    }
+         
+    if (RE_Count == 1000){  //Vary this based on timings
+     OE_Setpoint = 0; //Lower arm to base position
+    }
+   
+    
    /*
    Serial.print('\n');
    Serial.print(OE_PID_Output);
